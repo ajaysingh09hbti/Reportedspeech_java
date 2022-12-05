a5fdb881cc4cfa783b53605fa43cc7d37a9edec6
ajaysingh09hbti <education.me90@gmail.com>
2022-12-05 19:08:48 +0530

    added print statement

---
+++ src/Reported_Speech.java
@@ -0,0 +1,20 @@
+public class Reported_Speech {
+
+    public static void main (String[] args){
+
+        System.out.println(" In Reported Speech we study Direct and Indirect.");
+
+        System.out.println(" There are many rules to change Direct Speech into Indirect Speech.");
+
+        System.out.println(" 01. When the sentence of Direct Speech in Present Indefinite Tense and Simple Future Tense" +
+                " than we change say to into tell and in the place of inverted comma we use that.");
+
+        System.out.println(" 02. When the sentence of Direct Speech in Simple Present Tense and simple Future Tense than we can't change the Tense..  ");
+
+
+        System.out.println("I want to change some features on it.");
+
+
+
+    }
+}
